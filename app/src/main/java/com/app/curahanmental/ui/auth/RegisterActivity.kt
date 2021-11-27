a5603673d473684cf65a703a4f02657c4fbfc4e6
blitzkz23 <blitzaldykazu@gmail.com>
2021-11-27 17:50:20 +0700

    #1 setup package auth purpose

---
+++ app/src/main/java/com/app/curahanmental/ui/auth/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.app.curahanmental.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.curahanmental.R
+
+class RegisterActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_register)
+	}
+}
