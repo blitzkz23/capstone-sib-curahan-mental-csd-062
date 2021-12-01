a78a733cd20e873b8836398f67cf0694bef57cae
blitzkz23 <blitzaldykazu@gmail.com>
2021-12-01 09:29:02 +0700

    [#7] Fix the logout button not found

--- app/src/main/java/com/app/curahanmental/ui/main/MainActivity.kt
+++ app/src/main/java/com/app/curahanmental/ui/main/MainActivity.kt
@@ -3,21 +3,21 @@ package com.app.curahanmental.ui.main
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.app.curahanmental.R
 import com.app.curahanmental.databinding.ActivityMainBinding
+import com.app.curahanmental.databinding.FragmentHomeBinding
 import com.app.curahanmental.ui.auth.login.LoginActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
 	private lateinit var binding: ActivityMainBinding
+	private lateinit var homeBinding: FragmentHomeBinding
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
 		val navController = findNavController(R.id.nav_host_fragment_activity_main)
 		navView.setupWithNavController(navController)
 
-		binding.logoutButton.setOnClickListener {
+		homeBinding.logoutButton.setOnClickListener {
 			Firebase.auth.signOut()
 			Toast.makeText(this, "Kamu telah logout", Toast.LENGTH_SHORT).show()
 			startActivity(Intent(this, LoginActivity::class.java).also {
