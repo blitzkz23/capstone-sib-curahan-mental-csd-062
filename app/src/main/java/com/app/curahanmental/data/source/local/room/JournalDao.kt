6a0be500e5edcfe8410af465a5a23bb754b9909a
blitzkz23 <blitzaldykazu@gmail.com>
2021-12-01 07:17:06 +0700

    [#9] Write code for journal dao and database

--- app/src/main/java/com/app/curahanmental/data/source/local/room/JournalDao.kt
+++ app/src/main/java/com/app/curahanmental/data/source/local/room/JournalDao.kt
@@ -1,4 +1,21 @@
 package com.app.curahanmental.data.source.local.room
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.app.curahanmental.data.source.local.entity.JournalEntity
+import retrofit2.http.GET
+
+@Dao
 interface JournalDao {
+	@Insert(onConflict = OnConflictStrategy.REPLACE)
+	fun insertJournal(journal: JournalEntity)
+
+	@Delete
+	fun deleteJournal(journal: JournalEntity)
+
+	@Query("SELECT * FROM journal")
+	fun getAllJournal(): LiveData<List<JournalEntity>>
+
+	@Query("SELECT * FROM journal WHERE journal.id = :id")
+	fun getJournalById(id: Int): LiveData<JournalEntity>
 }
--- app/src/main/java/com/app/curahanmental/data/source/local/room/JournalDatabase.kt
+++ app/src/main/java/com/app/curahanmental/data/source/local/room/JournalDatabase.kt
@@ -1,4 +1,23 @@
 package com.app.curahanmental.data.source.local.room
 
-class JournalDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.app.curahanmental.data.source.local.entity.JournalEntity
+
+@Database(entities = [JournalEntity::class], version = 1, exportSchema = false)
+abstract class JournalDatabase : RoomDatabase() {
+
+	abstract fun journalDao(): JournalDao
+
+	@Volatile
+	private var instance: JournalDatabase? = null
+
+	fun getInstance(context: Context): JournalDatabase {
+		return synchronized(this) {
+			instance ?: Room.databaseBuilder(context, JournalDatabase::class.java, "journal.db")
+				.build()
+		}
+	}
 }
