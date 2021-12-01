3f585141a541004453b9fbbe654752ac5a1b2da3
Bagus Merta <merta.bagus24@gmail.com>
2021-12-01 12:08:43 +0800

    [#9] add Interface JournalDataSource

--- app/src/main/java/com/app/curahanmental/data/source/JournalDataSource.kt
+++ app/src/main/java/com/app/curahanmental/data/source/JournalDataSource.kt
@@ -1,4 +1,11 @@
 package com.app.curahanmental.data.source
 
+import androidx.lifecycle.LiveData
+import com.app.curahanmental.data.source.local.entity.JournalEntity
+
 interface JournalDataSource {
+    fun insertJournal(journalEntity: JournalEntity)
+    fun deleteJournal(journalEntity: JournalEntity)
+    fun getALlJournal(): LiveData<List<JournalEntity>>
+    fun getJournalById(id: Int): LiveData<JournalEntity>
 }
