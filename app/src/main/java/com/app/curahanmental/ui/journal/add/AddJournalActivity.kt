752c5211c4964f3eba972ae7e7c935f476c46983
blitzkz23 <blitzaldykazu@gmail.com>
2021-12-01 17:36:19 +0700

    [#10] Initialize activity for adding journal

---
+++ app/src/main/java/com/app/curahanmental/ui/journal/add/AddJournalActivity.kt
@@ -0,0 +1,12 @@
+package com.app.curahanmental.ui.journal.add
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.app.curahanmental.R
+
+class AddJournalActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_add_journal)
+	}
+}
---
+++ app/src/main/java/com/app/curahanmental/ui/journal/add/AddJournalViewModel.kt
@@ -0,0 +1,6 @@
+package com.app.curahanmental.ui.journal.add
+
+import androidx.lifecycle.ViewModel
+
+class AddJournalViewModel : ViewModel() {
+}
