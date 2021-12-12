522dd39e6b0879b7f1b0f459d85a7b675f908f35
blitzkz23 <blitzaldykazu@gmail.com>
2021-12-12 19:23:22 +0700

    [#10] Add sorting menu pop up

--- app/src/main/java/com/app/curahanmental/ui/auth/login/LoginViewModel.kt
+++ app/src/main/java/com/app/curahanmental/ui/auth/login/LoginViewModel.kt
@@ -1,7 +1,5 @@
 package com.app.curahanmental.ui.auth.login
 
-import android.content.Intent
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,18 +7,18 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 
-class LoginViewModel: ViewModel() {
-    private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
-    private val _authRes = MutableLiveData<Task<AuthResult>>()
+class LoginViewModel : ViewModel() {
+	private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+	private val _authRes = MutableLiveData<Task<AuthResult>>()
 
-    val currentUser = auth.currentUser
+	val currentUser = auth.currentUser
 
-    fun signInUser(email: String, password: String){
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener{
-                _authRes.postValue(it)
-            }
-    }
+	fun signInUser(email: String, password: String) {
+		auth.signInWithEmailAndPassword(email, password)
+			.addOnCompleteListener {
+				_authRes.postValue(it)
+			}
+	}
 
-    val authRes: LiveData<Task<AuthResult>> = _authRes
+	val authRes: LiveData<Task<AuthResult>> = _authRes
 }
--- app/src/main/java/com/app/curahanmental/ui/journal/JournalFragment.kt
+++ app/src/main/java/com/app/curahanmental/ui/journal/JournalFragment.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -15,6 +15,7 @@ import com.app.curahanmental.R
 import com.app.curahanmental.data.source.local.entity.JournalEntity
 import com.app.curahanmental.databinding.FragmentJournalBinding
 import com.app.curahanmental.ui.viemodel.ViewModelFactory
+import com.app.curahanmental.utils.JournalsSortType
 import com.google.android.material.button.MaterialButton
 
 class JournalFragment : Fragment() {
@@ -45,10 +46,28 @@ class JournalFragment : Fragment() {
 		binding.fabAdd.setOnClickListener { view ->
 			view.findNavController().navigate(R.id.action_navigation_journal_to_addJournalActivity)
 		}
-		getView()?.findViewById<MaterialButton>(R.id.sort_button)?.setOnClickListener {
-			Toast.makeText(activity, "Coba tombol", Toast.LENGTH_SHORT).show()
+		getView()?.findViewById<MaterialButton>(R.id.sort_button)?.setOnClickListener { view ->
+			showSortingPopUpMenu(view)
 		}
 		journalViewModel.journals.observe(viewLifecycleOwner, Observer(this::showJournalRecyclerView))
+	}
+
+	private fun showSortingPopUpMenu(view: View) {
+		PopupMenu(activity, view).run {
+			menuInflater.inflate(R.menu.sort_journal_menu, menu)
+
+			setOnMenuItemClickListener {
+				journalViewModel.sort(
+					when (it.itemId) {
+						R.id.bystress_level -> JournalsSortType.STRESS_LEVELS
+						R.id.bydate -> JournalsSortType.DATE
+						else -> JournalsSortType.ALL_JOURNALS
+					}
+				)
+				true
+			}
+			show()
+		}
 	}
 
 	private fun showJournalRecyclerView(journal: PagedList<JournalEntity>) {
