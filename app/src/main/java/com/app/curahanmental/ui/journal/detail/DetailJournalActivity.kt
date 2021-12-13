1de6795fbe92dffa5285e7188bfe7e2609f0e983
blitzkz23 <blitzaldykazu@gmail.com>
2021-12-13 19:40:20 +0700

    [#10] Detail activity initial layouting

--- app/src/main/java/com/app/curahanmental/ui/journal/detail/DetailJournalActivity.kt
+++ app/src/main/java/com/app/curahanmental/ui/journal/detail/DetailJournalActivity.kt
@@ -1,14 +1,31 @@
 package com.app.curahanmental.ui.journal.detail
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.app.curahanmental.R
+import com.app.curahanmental.databinding.ActivityDetailJournalBinding
 import com.app.curahanmental.utils.Constants.EXTRA_JOURNAL
+import com.google.android.material.button.MaterialButton
 
 class DetailJournalActivity : AppCompatActivity() {
+
+	private val binding: ActivityDetailJournalBinding by lazy { ActivityDetailJournalBinding.inflate(layoutInflater) }
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_detail_journal)
+		setContentView(binding.root)
+		loadActionBar()
+
+	}
+
+	private fun loadActionBar() {
+		findViewById<MaterialButton>(R.id.back_button).setOnClickListener {
+			super.onBackPressed()
+		}
+		findViewById<MaterialButton>(R.id.delete_button).setOnClickListener {
+			Toast.makeText(this, "Delete Journal?", Toast.LENGTH_SHORT).show()
+		}
 	}
 
 	companion object {
