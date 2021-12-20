b0a383a0251179d90419ba1f9a9c751589c2486c
blitzkz23 <blitzaldykazu@gmail.com>
2021-12-20 14:13:51 +0700

    [#21] Fix bugged commit

--- app/src/main/java/com/app/curahanmental/data/source/remote/entity/KindLinessMessageEntity.kt
+++
@@ -1,6 +0,0 @@
-package com.app.curahanmental.data.source.remote.entity
-
-data class KindLinessMessageEntity(
-    val messages: String? = "",
-    val time: Long?  = null
-)
--- app/src/main/java/com/app/curahanmental/data/source/remote/entity/KindlinessMessageEntity.kt
+++ app/src/main/java/com/app/curahanmental/data/source/remote/entity/KindlinessMessageEntity.kt
@@ -3,9 +3,9 @@ package com.app.curahanmental.data.source.remote.entity
 import com.google.firebase.database.Exclude
 
 data class KindlinessMessageEntity(
-	@get:Exclude
-	var id: String? = "",
-	val messages: String? = "",
-	val time: Long? = 0,
-	val poster: String? = ""
+    @get:Exclude
+    var id: String? = "",
+    val messages: String? = "",
+    val time: Long? = 0,
+    val poster: String? = ""
 )
