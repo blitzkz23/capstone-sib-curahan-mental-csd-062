3f0ab7e5339a97fe618501780954ddc98918f321
Bagus Merta <merta.bagus24@gmail.com>
2021-12-20 08:42:14 +0800

    [#21] modify users model to match the requirement, add message entity

--- app/src/main/java/com/app/curahanmental/data/source/remote/entity/UserEntity.kt
+++ app/src/main/java/com/app/curahanmental/data/source/remote/entity/UserEntity.kt
@@ -4,4 +4,5 @@ data class UserEntity(
 	val firstName: String? = "",
 	val lastName: String? = "",
 	val email: String? = "",
+	val kindliness_messages: HashMap<String, KindLinessMessageEntity>? = null,
 )
